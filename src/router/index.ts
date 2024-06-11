1ba6823e305936a504fd0dbcbbd62b9dd7a54f4e
IvanC <ivancavdar25@gmail.com>
2024-06-11 20:32:57 +0300

    feat: initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import ProductListView from '@/views/ProductTableView.vue';
+import ProductDetailView from '@/views/ProductDetailView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: ProductListView },
+    { path: '/product/:id', component: ProductDetailView },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
+  ]
+});
+
+export default router;
