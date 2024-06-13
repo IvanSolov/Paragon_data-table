370a3d231d2231530da9fc10f193bc0d81364894
IvanC <ivancavdar25@gmail.com>
2024-06-14 01:31:55 +0300

    refactor: remove garbage

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
-// Vuetify
-// import 'vuetify/styles'
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
 
 import vuetify from '@/plugins/vuetify';
 
