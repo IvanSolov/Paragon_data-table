d2794fdc89ba5f89e9f455b5c51b695434f0b646
IvanC <ivancavdar25@gmail.com>
2024-06-14 23:15:13 +0300

    feat: async load for ProductDetailView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import ProductListView from '@/views/ProductTableView.vue';
-import ProductDetailView from '@/views/ProductView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: ProductListView },
-    { path: '/product/:id', component: ProductDetailView },
+    { path: '/product/:id', component: () => import('@/views/ProductView.vue') },
     { path: '/:pathMatch(.*)*', redirect: '/' }
   ]
 });
