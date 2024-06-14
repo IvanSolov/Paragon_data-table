ae3b1bde59230fa4553a9188a888213b61e9d7e8
IvanSolov <ivanceavdar25@gmail.com>
2024-06-14 14:41:47 +0300

    chore: code style fix

--- src/services/apiServices/ProductApi/productService.spec.ts
+++ src/services/apiServices/ProductApi/productService.spec.ts
@@ -21,17 +21,28 @@ describe('Product API Service', () => {
       limit: 25
     };
 
-    (httpClient.get as ReturnType<typeof vi.fn>).mockResolvedValueOnce({ data: mockProductsResponse });
+    (httpClient.get as ReturnType<typeof vi.fn>).mockResolvedValueOnce({
+      data: mockProductsResponse
+    });
 
     const response = await fetchProducts({ itemsPerPage: 25, page: 1, search: '' });
     expect(response).toEqual(mockProductsResponse);
-    expect(httpClient.get).toHaveBeenCalledWith('/products', { params: { limit: 25, skip: 0, q: undefined } });
+    expect(httpClient.get).toHaveBeenCalledWith('/products', {
+      params: { limit: 25, skip: 0, q: undefined }
+    });
   });
 
   it('fetches product by id successfully', async () => {
-    const mockProductResponse = { id: 1, title: 'Product 1', description: 'Description 1', price: 100 };
+    const mockProductResponse = {
+      id: 1,
+      title: 'Product 1',
+      description: 'Description 1',
+      price: 100
+    };
 
-    (httpClient.get as ReturnType<typeof vi.fn>).mockResolvedValueOnce({ data: mockProductResponse });
+    (httpClient.get as ReturnType<typeof vi.fn>).mockResolvedValueOnce({
+      data: mockProductResponse
+    });
 
     const response = await fetchProductById(1);
     expect(response).toEqual(mockProductResponse);
@@ -41,12 +52,16 @@ describe('Product API Service', () => {
   it('handles error when fetching products', async () => {
     (httpClient.get as ReturnType<typeof vi.fn>).mockRejectedValueOnce(new Error('Network error'));
 
-    await expect(fetchProducts({ itemsPerPage: 25, page: 1, search: '' })).rejects.toThrow('Failed to fetch products: Error: Network error');
+    await expect(fetchProducts({ itemsPerPage: 25, page: 1, search: '' })).rejects.toThrow(
+      'Failed to fetch products: Error: Network error'
+    );
   });
 
   it('handles error when fetching product by id', async () => {
     (httpClient.get as ReturnType<typeof vi.fn>).mockRejectedValueOnce(new Error('Network error'));
 
-    await expect(fetchProductById(1)).rejects.toThrow('Failed to fetch product by id 1: Error: Network error');
+    await expect(fetchProductById(1)).rejects.toThrow(
+      'Failed to fetch product by id 1: Error: Network error'
+    );
   });
 });
